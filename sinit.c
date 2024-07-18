a715f29c7813d9fccb0e28858d5516c3f5a1a337
Timo Reichl <thetredev@gmail.com>
2024-07-18 23:25:40 +0200

    sinit.c: reformat code
    Signed-off-by: Timo Reichl <thetredev@gmail.com>

--- sinit.c
+++ sinit.c
@@ -7,8 +7,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define LEN(x)	(sizeof (x) / sizeof *(x))
-#define TIMEO	30
+#define LEN(x)    (sizeof (x) / sizeof *(x))
+#define TIMEO     30
 
 static void sigpoweroff(void);
 static void sigreap(void);
@@ -16,76 +16,72 @@ static void sigreboot(void);
 static void spawn(char *const []);
 
 static struct {
-	int sig;
-	void (*handler)(void);
+    int sig;
+    void (*handler)(void);
 } sigmap[] = {
-	{ SIGUSR1, sigpoweroff },
-	{ SIGCHLD, sigreap     },
-	{ SIGALRM, sigreap     },
-	{ SIGINT,  sigreboot   },
+    { SIGUSR1, sigpoweroff },
+    { SIGCHLD, sigreap     },
+    { SIGALRM, sigreap     },
+    { SIGINT,  sigreboot   },
 };
+
 
 #include "config.h"
 
 static sigset_t set;
 
-int
-main(void)
-{
-	int sig;
-	size_t i;
+int main(void) {
+    if (getpid() != 1) {
+        return 1;
+    }
 
-	if (getpid() != 1)
-		return 1;
-	chdir("/");
-	sigfillset(&set);
-	sigprocmask(SIG_BLOCK, &set, NULL);
-	spawn(rcinitcmd);
-	while (1) {
-		alarm(TIMEO);
-		sigwait(&set, &sig);
-		for (i = 0; i < LEN(sigmap); i++) {
-			if (sigmap[i].sig == sig) {
-				sigmap[i].handler();
-				break;
-			}
-		}
-	}
-	/* not reachable */
-	return 0;
+    chdir("/");
+    sigfillset(&set);
+    sigprocmask(SIG_BLOCK, &set, NULL);
+    spawn(rcinitcmd);
+
+    int sig;
+    size_t i;
+
+    while (1) {
+        alarm(TIMEO);
+        sigwait(&set, &sig);
+
+        for (i = 0; i < LEN(sigmap); i++) {
+            if (sigmap[i].sig == sig) {
+                sigmap[i].handler();
+                break;
+            }
+        }
+    }
+
+    /* not reachable */
+    return 0;
 }
 
-static void
-sigpoweroff(void)
-{
-	spawn(rcpoweroffcmd);
+static void sigpoweroff(void) {
+    spawn(rcpoweroffcmd);
 }
 
-static void
-sigreap(void)
-{
-	while (waitpid(-1, NULL, WNOHANG) > 0)
-		;
-	alarm(TIMEO);
+static void sigreap(void) {
+    while (waitpid(-1, NULL, WNOHANG) > 0);
+    alarm(TIMEO);
 }
 
-static void
-sigreboot(void)
-{
-	spawn(rcrebootcmd);
+static void sigreboot(void) {
+    spawn(rcrebootcmd);
 }
 
-static void
-spawn(char *const argv[])
-{
-	switch (fork()) {
-	case 0:
-		sigprocmask(SIG_UNBLOCK, &set, NULL);
-		setsid();
-		execvp(argv[0], argv);
-		perror("execvp");
-		_exit(1);
-	case -1:
-		perror("fork");
-	}
+static void spawn(char *const argv[]) {
+    switch (fork()) {
+    case 0:
+        sigprocmask(SIG_UNBLOCK, &set, NULL);
+        setsid();
+        execvp(argv[0], argv);
+
+        perror("execvp");
+        _exit(1);
+    case -1:
+        perror("fork");
+    }
 }
