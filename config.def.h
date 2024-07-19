5877e7ed8441af19fec8bbe0e146fa0fb15c8f5f
Timo Reichl <thetredev@gmail.com>
2024-07-19 19:43:09 +0200

    config.def.h: include <stddef.h> to explicitly declare NULL
    Signed-off-by: Timo Reichl <thetredev@gmail.com>

--- config.def.h
+++ config.def.h
@@ -1,4 +1,5 @@
 /* See LICENSE file for copyright and license details. */
+#include <stddef.h>
 
 static char *const rcinitcmd[]     = { "/bin/rc.init", NULL };
 static char *const rcrebootcmd[]   = { "/bin/rc.shutdown", "reboot", NULL };
